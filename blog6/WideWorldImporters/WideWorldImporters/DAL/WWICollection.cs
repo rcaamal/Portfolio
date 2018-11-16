99124253d6d240f4dadcdcb6b73b5c64d23ea066
Randy Caamal <rcaamalcastillo13@mail.wou.edu>
2018-11-15 22:46:37 -0800

    added the MVC

---
+++ blog6/WideWorldImporters/WideWorldImporters/DAL/WWICollection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WideWorldImporters.Models;
+using System.Data.Entity;
+
+namespace WideWorldImporters.DAL
+{
+    public class WWICollection : DbContext
+    {
+        // GET: WWICollection
+        public WWICollection() : base("WWICollection")
+        {
+            
+        }
+    }
+}
---
+++ blog6/WideWorldImporters/WideWorldImporters/Models/SearchContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace WideWorldImporters.Models
+{
+    public class SearchContext
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        public string Search { get; set; }
+
+
+
+
+
+        public override string ToString()
+        {
+            return $"{base.ToString()}: {Search}   ";
+
+        }
+    }
+}
