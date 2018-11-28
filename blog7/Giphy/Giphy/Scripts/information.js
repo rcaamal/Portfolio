50a0acfc10c457ccb03a58e0e6281345ae45a1cc
Randy Caamal <rcaamalcastillo13@mail.wou.edu>
2018-11-27 23:58:55 -0800

    added the MVC

---
+++ blog7/Giphy/Giphy/Scripts/information.js
@@ -0,0 +1,39 @@
+﻿var searchString = '';
+var requestLink = '';
+var key = $("#key").data("value");
+
+function main() 
+{
+    $("#textbar").on('type', function () {
+        if (!($("#sbar").val())) {
+            console.log("Nothing entered.");
+        }
+        else {
+            
+            searchString = $("#sbar").val().replace(" ", "+");
+            $("#sbar").val('');
+            requestLink = "http://api.giphy.com/v1/gifs/search?q=" + searchString + "&api_key=" + key;
+
+            $(".gifRow").html(null);
+
+          
+
+            $.ajax({
+                type: 'POST',
+                url: '/Home/Index/',
+                data: { search: searchString },
+                success: function () {
+                    console.log("Yay");
+                }
+            });
+        }
+
+        
+    })
+}
+
+
+$(document).ready(main);
+
+
+
